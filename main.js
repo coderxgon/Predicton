e824a2589c6e88a5b1ba729f5e948c6e63bab0f0
coderxgon <glenalberth2011@gmail.com>
2024-11-03 12:50:46 +0000

    ff

--- main.js
+++
@@ -1,9 +0,0 @@
-function blockAccess() {
-  // Redirect to a different page after 0.3 seconds
-  setTimeout(() => {
-      window.location.href = 'maintenance.html'; // Change to your desired URL
-  }, 300); // 300 milliseconds = 0.3 seconds
-}
-
-// Call the blockAccess function when the page loads
-window.onload = blockAccess;
